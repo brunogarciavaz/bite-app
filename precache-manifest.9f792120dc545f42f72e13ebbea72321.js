ca35f25951beb60e1d68aae92d8177d4c6a7cfaa
brunogarciavaz <brunogarciacosta@gmail.com>
2020-05-25 20:35:09 -0300

    Updates

---
+++ precache-manifest.9f792120dc545f42f72e13ebbea72321.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eae6b1f3ec0384b5c9ca2511731c2d5",
+    "url": "/bite-app/index.html"
+  },
+  {
+    "revision": "d21ef970772d46b907bf",
+    "url": "/bite-app/static/css/2.1174ccc1.chunk.css"
+  },
+  {
+    "revision": "d21ef970772d46b907bf",
+    "url": "/bite-app/static/js/2.d91bb72c.chunk.js"
+  },
+  {
+    "revision": "a1a88636b752ef668fe9699df3c2d360",
+    "url": "/bite-app/static/js/2.d91bb72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a0c2a2a9ab1fbbb18c",
+    "url": "/bite-app/static/js/main.855b1025.chunk.js"
+  },
+  {
+    "revision": "024ccfe65071b6ab69e9",
+    "url": "/bite-app/static/js/runtime-main.ed15ef99.js"
+  },
+  {
+    "revision": "ff0fece3160f2092b75ee3b65e5c6b68",
+    "url": "/bite-app/static/media/check-solid.ff0fece3.svg"
+  },
+  {
+    "revision": "7ed63aa407e78280366deaf9bc3ae29c",
+    "url": "/bite-app/static/media/logo.7ed63aa4.png"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "/bite-app/static/media/plus-solid.b99e8ee2.svg"
+  },
+  {
+    "revision": "8a98a2444790708bd5733be81a0b400a",
+    "url": "/bite-app/static/media/shopping-bag.8a98a244.svg"
+  }
+]);
