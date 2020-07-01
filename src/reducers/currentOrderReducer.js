71eef4aa891558c01979d324d275b222ccd2d386
Bruno Vaz <brunogarciacosta@gmail.com>
2020-07-01 15:02:30 -0300

    Fix Redux's compose error

--- src/reducers/currentOrderReducer.js
+++ src/reducers/currentOrderReducer.js
@@ -2,19 +2,6 @@
 import { combineState } from '../helpers';
 
 
-// function combineState(state, action, subtract) {
-// 	return Object.keys(state)
-// 	.reduce(function(newObj, k) {
-// 		if (action[k] && typeof action[k] === 'object') {
-// 			newObj[k] = combineState(state[k], action[k], subtract)
-// 			return newObj
-// 		}
-// 		newObj[k] = subtract ? (state[k] || 0) - (action[k] || 0) : (state[k] || 0) + (action[k] || 0)
-// 		return newObj;
-// 	}, {})
-//
-// }
-
 const initialState = {
   orderData: {
 		nutrients: {protein: null, carbohydrate: null, fats: null, kcal: null},
